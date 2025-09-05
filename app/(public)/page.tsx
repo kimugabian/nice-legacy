afeb42dbc4a58560eee6bba6f4ac11f10b77310a
Hafizh24 <hafizhcbs@gmail.com>
2025-09-05 12:57:05 +0700

    feat:create landing page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,39 +1,83 @@
-"use client";
+import { Badge } from "@/components/ui/badge";
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
 
-import { Button } from "@/components/ui/button";
-import { ThemeToggle } from "@/components/ui/themeToggle";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
-import { toast } from "sonner";
+interface FeatureProps {
+  title: string;
+  description: string;
+  icon: string;
+}
+
+const features: FeatureProps[] = [
+  {
+    title: "Comprehensive Courses",
+    description:
+      "Access a wide range of courses from various subjects and topics, helping you learn and grow.",
+    icon: "📚",
+  },
+  {
+    title: "Interactive Learning",
+    description:
+      "Engage with interactive content, quizzes, and assignments to enhance your learning experience.",
+    icon: "🎉",
+  },
+  {
+    title: "Personalized Learning",
+    description:
+      "Tailor your learning experience based on your interests and learning goals.",
+    icon: "🎯",
+  },
+  {
+    title: "Community Engagement",
+    description:
+      "Connect with fellow learners, share knowledge, and collaborate with like-minded individuals.",
+    icon: "👥",
+  },
+];
 
 export default function Home() {
-  const router = useRouter();
-  const { data: session } = authClient.useSession();
-
-  const signOut = async () => {
-    await authClient.signOut({
-      fetchOptions: {
-        onSuccess: () => {
-          router.push("/");
-          toast.success("Sign Out successfully");
-        },
-      },
-    });
-  };
-
   return (
-    <div>
-      <h1>Hello world</h1>
-      <ThemeToggle />
+    <>
+      <section className="relative py-20">
+        <div className="flex flex-col items-center text-center space-y-8">
+          <Badge variant="outline">The Future of Online Education</Badge>
+          <h1 className="text-4xl md:text-6xl font-bold tracking-tight">
+            Elevate your Learning Experience{" "}
+          </h1>
+          <p className="max-w-[700px] text-muted-foreground md:text-xl">
+            Discover a new way to learn with our modern,interactive learning
+            management system. Access high-quality courses anytime, anywhere.
+          </p>
 
-      {session ? (
-        <div>
-          <p>You are signed in as {session.user.name}</p>
-          <Button onClick={signOut}>Sign out</Button>
+          <div className="flex flex-col sm:flex-row gap-4 mt-8">
+            <Link className={buttonVariants({ size: "lg" })} href="/courses">
+              Explore Courses
+            </Link>
+
+            <Link
+              className={buttonVariants({ size: "lg", variant: "outline" })}
+              href="/login"
+            >
+              Sign in
+            </Link>
+          </div>
         </div>
-      ) : (
-        <p>You are not signed in</p>
-      )}
-    </div>
+      </section>
+
+      <section className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-32">
+        {features.map((feature, index) => (
+          <Card key={index} className="hover:shadow-lg transition-shadow">
+            <CardHeader>
+              <div className="text-4xl mb-4">{feature.icon}</div>
+              <CardTitle>{feature.title}</CardTitle>
+            </CardHeader>
+            <CardContent>
+              <p className="text-muted-foreground">{feature.description}</p>
+            </CardContent>
+          </Card>
+        ))}
+      </section>
+    </>
   );
 }
