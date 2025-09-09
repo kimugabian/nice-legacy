c0fb7544b3b2c3f03cd50159f21ab3cf5e31204e
Hafizh24 <hafizhcbs@gmail.com>
2025-09-09 13:12:23 +0700

    feat:stripe integration

--- app/(public)/courses/[slug]/page.tsx
+++ app/(public)/courses/[slug]/page.tsx
@@ -1,7 +1,6 @@
 import { getCourse } from "@/app/data/course/get-course";
 import { RenderDescription } from "@/components/rict-text-editor/RenderDescription";
 import { Badge } from "@/components/ui/badge";
-import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import {
   Collapsible,
@@ -20,6 +19,10 @@ import {
 } from "@tabler/icons-react";
 import { CheckIcon } from "lucide-react";
 import Image from "next/image";
+import { checkIfCourseBought } from "@/app/data/user/user-is-enrolled";
+import Link from "next/link";
+import { EnrollmentButton } from "./_components/EnrollmentButton";
+import { buttonVariants } from "@/components/ui/button";
 
 type Params = Promise<{ slug: string }>;
 
@@ -27,6 +30,7 @@ export default async function SlugPage({ params }: { params: Params }) {
   const { slug } = await params;
   const course = await getCourse(slug);
   const thumbnailUrl = useConstructUrl(course.fileKey);
+  const isEnrolled = await checkIfCourseBought(course.id);
 
   return (
     <div className="grid grid-cols-1 gap-8 lg:grid-cols-3 mt-5">
@@ -256,7 +260,17 @@ export default async function SlugPage({ params }: { params: Params }) {
                 </ul>
               </div>
 
-              <Button className="w-full">Enroll now!</Button>
+              {isEnrolled ? (
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({ className: "w-full" })}
+                >
+                  Watch Course
+                </Link>
+              ) : (
+                <EnrollmentButton courseId={course.id} />
+              )}
+
               <p className="mt-3 text-center text-xs text-muted-foreground">
                 30-day money back guarantee
               </p>
