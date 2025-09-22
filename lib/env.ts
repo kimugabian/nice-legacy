dbb81309eacaece899e3c3b293ab5aded3050b38
Hafizh24 <hafizhcbs@gmail.com>
2025-09-22 17:37:15 +0700

    remove unnecessary file

--- lib/env.ts
+++ lib/env.ts
@@ -6,8 +6,6 @@ export const env = createEnv({
     DATABASE_URL: z.string().url(),
     BETTER_AUTH_SECRET: z.string().min(1),
     BETTER_AUTH_URL: z.string().url(),
-    GITHUB_CLIENT_ID: z.string().min(1),
-    GITHUB_SECRET: z.string().min(1),
     GOOGLE_CLIENT_ID: z.string().min(1),
     GOOGLE_CLIENT_SECRET: z.string().min(1),
     RESEND_API_KEY: z.string().min(1),
@@ -17,8 +15,6 @@ export const env = createEnv({
     AWS_ENDPOINT_URL_S3: z.string().min(1),
     AWS_ENDPOINT_URL_IAM: z.string().min(1),
     AWS_REGION: z.string().min(1),
-    STRIPE_SECRET_KEY: z.string().min(1),
-    STRIPE_WEBHOOK_SECRET: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_S3_BUCKET_NAME_IMAGES: z.string().min(1),
--- lib/stripe.ts
+++
@@ -1,9 +0,0 @@
-import "server-only";
-
-import Stripe from "stripe";
-import { env } from "./env";
-
-export const stripe = new Stripe(env.STRIPE_SECRET_KEY, {
-  apiVersion: "2025-08-27.basil",
-  typescript: true,
-});
